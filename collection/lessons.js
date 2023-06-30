796a32dbfff754877010c18341212f49fb368ecb
ekendirichukwu <44375814+ekendirichukwu@users.noreply.github.com>
2023-06-30 23:26:59 +0400

    Add files via upload

---
+++ collection/lessons.js
@@ -0,0 +1,92 @@
+[
+    {
+        "_id": "6478fd7f540dfee905d4a5db",
+        "id": 1,
+        "title": "Psychology",
+        "location": "Ghana",
+        "price": 110,
+        "image": "Psychology.jpg",
+        "availableInventory": 5
+    },
+    {
+        "_id": "64790058540dfee905d4a5ec",
+        "id": 9,
+        "title": "Mathematics",
+        "location": "Canada",
+        "price": 120,
+        "image": "mathematics.jpg",
+        "availableInventory": 8
+    },
+    {
+        "_id": "6478fec3540dfee905d4a5df",
+        "id": 3,
+        "title": "Chemistry",
+        "location": "New York",
+        "price": 180,
+        "image": "chemistry.jpg",
+        "availableInventory": 15
+    },
+    {
+        "_id": "6478ff5f540dfee905d4a5e3",
+        "id": 5,
+        "title": "Java Programming Language",
+        "location": "Nigeria",
+        "price": 40,
+        "image": "java.jpg",
+        "availableInventory": 8
+    },
+    {
+        "_id": "6478ff36540dfee905d4a5e1",
+        "id": 4,
+        "title": "Geography",
+        "location": "California",
+        "price": 80,
+        "image": "geography.jpg",
+        "availableInventory": 7
+    },
+    {
+        "_id": "6478ff9e540dfee905d4a5e6",
+        "id": 6,
+        "title": "Medicine",
+        "location": "London",
+        "price": 90,
+        "image": "medicine.jpg",
+        "availableInventory": 5
+    },
+    {
+        "_id": "6479001e540dfee905d4a5e8",
+        "id": 7,
+        "title": "Python Programming ",
+        "location": "Japan",
+        "price": 60,
+        "image": "python.jpg",
+        "availableInventory": 8
+    },
+    {
+        "_id": "6479003a540dfee905d4a5ea",
+        "id": 8,
+        "title": "Economic",
+        "location": "Gambia",
+        "price": 78,
+        "image": "economics.jpg",
+        "availableInventory": 6
+    },
+    {
+        "_id": "64790071540dfee905d4a5ee",
+        "id": 10,
+        "title": "Architecture",
+        "location": "Sincinati",
+        "price": 89,
+        "image": "architecture.jpg",
+        "availableInventory": 9
+    },
+    {
+        "_id": "6478fe7b540dfee905d4a5dd",
+        "id": 2,
+        "title": "Art",
+        "location": "Kano",
+        "price": 100,
+        "image": "art.jpg",
+        "availableInventory": 6
+    }
+]
